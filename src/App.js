472085fdb746779b0b3919d4383babbe7a1eb2b5
Baigazy Torobekov <baigazy47@gmail.com>
2018-01-16 17:00:50 +0600

    Создал директорию Person и вниури нее создал файл Person.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import RandomNumbers from "./Person/Person.js";
 
 class App extends Component {
   render() {
@@ -10,12 +11,25 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          Hello, this is my new React app!
-        </p>
+        <RandomNumbers name="New Number"/>
+        <RandomNumbers random="x"/>
       </div>
     );
   }
 }
 
 export default App;
+// class App extends Component {
+//     render() {
+//         return (
+//             <div className="App">
+//                 <header className="App-header">
+//                     <img src={logo} className="App-logo" alt="logo" />
+//                     <h1 className="App-title">Welcome to React</h1>
+//                 </header>
+//                 <Person name="John" age="29" />
+//                 <Person name="John" age="29" />
+//             </div>
+//         );
+//     }
+// }
