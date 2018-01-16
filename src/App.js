9538be525edffafbb9df6abc4477277ca2aa8176
Baigazy Torobekov <baigazy47@gmail.com>
2018-01-16 18:54:25 +0600

    Создал файл number.js и директорию Numbers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import RandomNumbers from "./Person/Person.js";
+// import RandomNumbers from "./Person/Person.js";
+import Number from './Numbers/Number'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <RandomNumbers name="New Number"/>
-        <RandomNumbers random="x"/>
-      </div>
-    );
-  }
+    state = {
+        numbers: [0, 0, 0, 0, 0]
+    }
+    handleClick = () => {
+        let i = 5;
+        let numbers = [];
+        while (i > 0) {
+            const rand = Math.floor(Math.random() * (37 - 5) + 5);
+            if (numbers.indexOf() === -1) {
+                numbers.push(rand);
+                i--;
+            }
+        }
+        this.setState({numbers});
+    }
+
+    render() {
+        return (
+            <div>
+                <button className="btn_numbers" onClick={this.handleClick}>New Numbers</button>
+                <Number number={this.state.numbers[0]} />
+                <Number number={this.state.numbers[1]} />
+                <Number number={this.state.numbers[2]} />
+                <Number number={this.state.numbers[3]} />
+                <Number number={this.state.numbers[4]} />
+            </div>
+        );
+    }
+
 }
 
 export default App;
---
+++ src/Numbers/Number.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Number = props => (
+    <div>
+        <span>{props.number}</span>
+    </div>
+)
+
+export default Number;
